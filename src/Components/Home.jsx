3c14eaa93f30ed570c6b55ff9dcab9b4213d0a3b
SohailaMohammd <160627240+SohailaMohammd@users.noreply.github.com>
2025-10-27 20:33:32 +0300

    new

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -14,8 +14,8 @@ const Home = () => {
                 <div class="carousel-inner">
                     <div class="carousel-item active">
                         <img src={heroImage} class="d-block w-100" height={660} style={{ objectFit: "cover" }} alt="heroImage" />
-                        <div class="carousel-caption d-flex text-start justify-content-start d-none d-md-block" style={{ position: "absolute", backdropFilter: "blur(3px)",padding:"12px", top: "50%", left: "10%", transform: "translateY(-50%)" }}>
-                            <h5 className="card-title fst-italic fw-bold text fw-lighter headerHome">Welcome to a World Made for You</h5>
+                        <div class="carousel-caption d-flex text-start justify-content-start d-none d-md-block" style={{ position: "absolute", backdropFilter: "blur(3px)", padding: "12px", top: "50%", left: "10%", transform: "translateY(-50%)" }}>
+                            <h5 className="card-title fst-italic fw-bold text fw-lighter headerHome">Welcome to a <span className="navbar-brand">World</span> Made for You</h5>
                             <p className="card-text fw-bold fs-5 fw-lighter d-none d-sm-block">Welcome to a world where shopping feels effortless— a place made to inspire your style, and help you find what truly fits your vibe. Enter, explore freely, and make every moment unique to you.</p>
                         </div>
                     </div>
